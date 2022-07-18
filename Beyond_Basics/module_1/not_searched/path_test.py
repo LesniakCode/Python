6a0c708636332b65eea507e8f6f8ed18e4e0685c
MAster <edwardopl@gmail.com>
2022-07-18 14:30:31 +0200

    Packages_struct

--- Beyond_Basics/module_1/not_searched/path_test.py
+++ Beyond_Basics/module_1/not_searched/path_test.py
@@ -1,2 +1,10 @@
+'''
+Test path search - open python interpreter in module_1 path
+Try import path_test - fail
+manipulate sys.path to add folder 'not_searched' then import - pass
+
+Conclusion: import of modules can be manipulated during runtime 
+by modify sys.path, but it's not solid
+'''
 def found(): 
     print("Python found me!")
